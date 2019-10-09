27af4046691916f6dd01c32fe8841ab77a79eea8
Daily dose of hacking <52329418+TTVralph@users.noreply.github.com>
2019-10-09 01:09:19 -0300

    Update image recognition.py

--- image recognition.py
+++ image recognition.py
@@ -1,7 +1,6 @@
 import io
 import os
 import sys
-
 import json
 from watson_developer_cloud import VisualRecognitionV3
 
@@ -9,17 +8,8 @@ visual_recognition = VisualRecognitionV3(
     '2018-03-19',
     iam_apikey='jg27XMfTEyZ_TumtVhf70OloEZQmpI1o0hZbvjCLko2N')
 
-#if press U switch to url2
 url = 'https://vignette.wikia.nocookie.net/fortnite/images/3/3e/Kevin_the_cube.png/revision/latest?cb=20181221204823'
-url2 ='https://vignette.wikia.nocookie.net/youtube/images/7/7b/LazarBeamFace.jpg/revision/latest?cb=20190110194323'
-
 classes_result = visual_recognition.classify(url=url).get_result()
 print(json.dumps(classes_result, indent=2))
-
-
-
-# print("Please type the file path of the image file you would like to scan (from ~)")
-# filepath = sys.stdin.readline()
-# filepath = filepath.strip()
 
 input("Press ENTER to exit ;)")
